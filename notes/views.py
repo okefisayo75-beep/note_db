9c38b3f9d72b579c509cceac62e4a992cafad7c4
Isaac Abiodun <engrisaac1234@gmail.com>
2025-10-17 16:09:41 +0100

    first fix

--- notes/views.py
+++ notes/views.py
@@ -2,10 +2,26 @@ from django.shortcuts import render
 from .models import Note #db
 from .serializers import NoteSerializer
 from rest_framework import viewsets
-# Create your views here.
+from django.db.models import Q
+
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    ##DB creation......
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search = self.request.query_params.get('search', None)
+        
+        if search:
+            queryset = queryset.filter(
+                Q(title__icontains=search) | 
+                Q(content__icontains=search) 
+            )
+        
+        return queryset
+
+
 
 
